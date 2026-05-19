c0b247954fdec89a2e834da9bf98dbb649d7889d
Kurt <kurt.hvejsel@gmail.com>
2026-05-19 22:03:54 +1000

    initial commit

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Installing Packages..."
+sudo pacman -S --needed - <packages.txt
+
+echo "Creating Directories..."
+mkdir -p ~/.config/kitty/
+mkdir -p ~/.config/hypr/
+mkdir -p ~/.config/nvim/lua/
+mkdir -p ~/.config/waybar/
+
+echo "Creating symlinks..."
+ln -sf ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -sf ~/dotfiles/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -sf ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ~/dotfiles/nvim/lazyvim.json ~/.config/nvim/lazyvim.json
+ln -sf ~/dotfiles/nvim/lua/config/ ~/.config/nvim/lua/config/
+ln -sf ~/dotfiles/nvim/lua/plugins/ ~/.config/nvim/lua/plugins/
+ln -sf ~/dotfiles/waybar/config ~/.config/waybar/config
+ln -sf ~/dotfiles/waybar/style.css ~/.config/waybar/style.css
+ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml
+ln -sf ~/dotfiles/btop/ ~/.config/btop/
